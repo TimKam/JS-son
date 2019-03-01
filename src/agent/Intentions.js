24a66f8e13fb0a3d9afc356e83de8827d1a5fd05
Timotheus Kampik <timotheus.kampik@gmail.com>
2019-03-01 23:23:34 +0100

    rename variable (use full words to be more explicit)

--- src/agent/Intentions.js
+++ src/agent/Intentions.js
@@ -2,12 +2,12 @@
  * JS-son agent intentions generator
  * @param {object} beliefs the agent's current beliefs
  * @param {object} desires the agent's desires (from which the intentions are filtered)
- * @param {function} preferenceFunctionGen
+ * @param {function} preferenceFunctionGenerator
  * @returns {array} JS-son agent intentions
  */
-const Intentions = (beliefs, desires, preferenceFunctionGen) => {
+const Intentions = (beliefs, desires, preferenceFunctionGenerator) => {
   const intentions = {}
-  const intentionKeys = Object.keys(desires).filter(preferenceFunctionGen(beliefs, desires))
+  const intentionKeys = Object.keys(desires).filter(preferenceFunctionGenerator(beliefs, desires))
   intentionKeys.forEach(intentionKey => (intentions[intentionKey] = desires[intentionKey](beliefs)))
   return intentions
 }
